ce827853040fcff8a7d1172080b8b9238e0cc00b
Oswaldo Pereira <109630781+OswaldoAPF@users.noreply.github.com>
2024-12-01 11:31:04 -0300

    chore: primer commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <drawer-component>
+
+    </drawer-component>
+
+
+</template>
+
+<script>
+import './components/lit/drawer'; 
+import { options } from './data/options';
+
+export default {
+  data() {
+    return {
+      options,
+    };
+  },
+
+};
+</script>
+
